31a9f377fc19fd55b48e590fb71812665c65bdeb
mirzayevt23 <mirzayevt23@gmail.com>
2024-12-08 15:14:09 +0500

    setting responsive

--- src/pages/admin-page/components/Dashbord.jsx
+++ src/pages/admin-page/components/Dashbord.jsx
@@ -1,56 +1,68 @@
-import {useState} from "react";
-// import React, { useState } from 'react';
-import { AimOutlined, FileProtectOutlined,  } from '@ant-design/icons';
-import { Menu } from 'antd';
-import {NavLink} from "react-router";
-const items = [
-    {
-        key: 'sub1',
-        label: <NavLink to={"/"}>Map</NavLink>,
-        icon: <AimOutlined />,
+import { useState } from "react";
+import { AimOutlined, FileProtectOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
 
-
-    },
-    {
-        key: 'sub2',
-        label: <NavLink to={"/user-info"}>Client info</NavLink>,
-        icon: <i className={"fa-solid fa-user"} />,
-
-    },
-
-
-    {
-        key: 'sub4',
-        label: <NavLink to={"/product-list"}>Product list</NavLink>,
-        icon: <FileProtectOutlined />,
-
-    },
-];
-const Dashbord = () => {
-    const [theme, setTheme] = useState('dark');
-    const [current, setCurrent] = useState('1');
+const Dashbord = ({ mobile }) => {
+    const [current, setCurrent] = useState("sub1");
 
     const onClick = (e) => {
-        console.log('click ', e);
         setCurrent(e.key);
     };
-    return (
-        <>
 
-            <br />
-            <br />
-            <Menu
-                theme={theme}
-                onClick={onClick}
-                style={{
-                    width: 256,
-                }}
-                defaultOpenKeys={['sub1']}
-                selectedKeys={[current]}
-                mode="inline"
-                items={items}
-            />
-        </>
+    const items = [
+        {
+            key: "sub1",
+            label: <NavLink to="/">Map</NavLink>,
+            icon: <AimOutlined />,
+        },
+        {
+            key: "sub2",
+            label: <NavLink to="/user-info">Client Info</NavLink>,
+            icon: <i className="fa-solid fa-user" />,
+        },
+        {
+            key: "sub4",
+            label: <NavLink to="/product-list">Product List</NavLink>,
+            icon: <FileProtectOutlined />,
+        },
+    ];
+
+    return (
+        <div>
+            {/* Mobil versiyasi uchun pastki menyu */}
+            {mobile ? (
+                <div className="flex justify-around py-2">
+                    {items.map((item) => (
+                        <NavLink
+                            key={item.key}
+                            to={item.label.props.to}
+                            className={({ isActive }) =>
+                                `flex flex-col items-center justify-center text-sm ${
+                                    isActive ? "text-[#00aaff]" : "text-white"
+                                }`
+                            }
+                        >
+                            {item.icon}
+                            <span>{item.label.props.children}</span>
+                        </NavLink>
+                    ))}
+                </div>
+            ) : (
+                <Menu
+                    theme="dark"
+                    onClick={onClick}
+                    style={{
+                        width: "100%",
+                    }}
+                    defaultOpenKeys={["sub1"]}
+                    selectedKeys={[current]}
+                    mode="inline"
+                    items={items}
+                />
+            )}
+        </div>
     );
 };
+
 export default Dashbord;
--- src/pages/admin-page/pages/Admin.jsx
+++ src/pages/admin-page/pages/Admin.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import Dashbord from '../components/Dashbord.jsx'
-import Map from "../components/Map.jsx"
-import {Outlet} from "react-router";
+import Dashbord from "../components/Dashbord.jsx";
+import { Outlet } from "react-router";
+
 const AdminPage = () => {
     return (
-        <div className="max-w-[1536px] mx-auto fixed top-0 left-0 right-0 bottom-0 flex">
-            {/* Dashbord qismi */}
-            <div className="bg-[#041025] basis-1/6 min-h-[100vh] text-white overflow-y-auto z-30">
+        <div className="max-w-[1536px] mx-auto  top-0 left-0 right-0 bottom-0 flex flex-col lg:flex-row">
+
+            <div className="bg-[#041025] basis-1/6 min-h-[100vh] text-white overflow-y-auto z-30 hidden lg:block">
                 <Dashbord />
-            </div>{/* Map qismi */}
+            </div>
+
             <div className="basis-5/6 min-h-[100vh]">
                 <Outlet />
+            </div>
+
+            <div className="lg:hidden fixed bottom-0 left-0 w-full bg-[#041025] text-white z-50 shadow-lg">
+                <Dashbord mobile />
             </div>
         </div>
     );
